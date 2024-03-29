939382bac2ede4a22a6ca7f194bb4e1d25ac6883
jeanlucngo <thanh.ngo@etu.univ-amu.fr>
2024-03-29 14:53:51 +0100

    First commit

---
+++ src/index.js
@@ -0,0 +1,52 @@
+import { Engine, FreeCamera, HemisphericLight, MeshBuilder, Scene, Vector3 } from "@babylonjs/core";
+import { Insepector } from "@babylonjs/inspector";
+
+let engine;
+let canvas;
+
+window.onload = () => {
+    canvas = document.getElementById("renderCanvas");
+    engine = new Engine(canvas, true);
+    let scene = createScene(); 
+
+    Insepector.Show(scene, {});
+
+    engine.runRenderLoop(function () {
+        scene.render();
+        });
+
+    window.addEventListener("resize", function () {
+        engine.resize();
+        });
+}
+
+var createScene = function () {
+    // This creates a basic Babylon Scene object (non-mesh)
+    var scene = new Scene(engine);
+
+    // This creates and positions a free camera (non-mesh)
+    var camera = new FreeCamera("camera1", new Vector3(0, 5, -10), scene);
+
+    // This targets the camera to scene origin
+    camera.setTarget(Vector3.Zero());
+
+    // This attaches the camera to the canvas
+    camera.attachControl(canvas, true);
+
+    // This creates a light, aiming 0,1,0 - to the sky (non-mesh)
+    var light = new HemisphericLight("light", new Vector3(0, 1, 0), scene);
+
+    // Default intensity is 1. Let's dim the light a small amount
+    light.intensity = 0.7;
+
+    // Our built-in 'sphere' shape.
+    var sphere = MeshBuilder.CreateSphere("sphere", {diameter: 2, segments: 32}, scene);
+
+    // Move the sphere upward 1/2 its height
+    sphere.position.y = 1;
+
+    // Our built-in 'ground' shape.
+    var ground = MeshBuilder.CreateGround("ground", {width: 6, height: 6}, scene);
+
+    return scene;
+};
