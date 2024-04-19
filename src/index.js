5c4a995e139d06528fc9fe2bb105c2e60d689736
Manelmk <91433284+Manelmk@users.noreply.github.com>
2024-04-19 14:49:41 +0200

    init index.js

--- src/index.js
+++ src/index.js
@@ -1,52 +1,66 @@
-import { Engine, FreeCamera, HemisphericLight, MeshBuilder, Scene, Vector3 } from "@babylonjs/core";
-import { Insepector } from "@babylonjs/inspector";
+import { Engine, FreeCamera, HemisphericLight, MeshBuilder, Scene, Vector3 } 
+from "@babylonjs/core"; 
+ 
+window.onload = () => { 
+    console.log('Hello World!'); 
+ 
+    const canvas = document.getElementById("renderCanvas"); 
+    let engine = new Engine(canvas, true); 
+ 
+    const createScene = function () { 
+        const scene = new Scene(engine); 
+        const sphere = MeshBuilder.CreateSphere("sphere",  
+            {diameter: 2, segments: 32}, scene); 
+        sphere.position.y = 1; 
 
-let engine;
-let canvas;
+        // Configuration de la caméra
+        const camera = new FreeCamera("camera1",  
+            new Vector3(0, 5, -10), scene); 
+        camera.setTarget(sphere.position);  // Réglage initial de la cible de la caméra
 
-window.onload = () => {
-    canvas = document.getElementById("renderCanvas");
-    engine = new Engine(canvas, true);
-    let scene = createScene(); 
+        camera.attachControl(canvas, true); 
+        const light = new HemisphericLight("light",  
+            new Vector3(0, 1, 0), scene); 
+        light.intensity = 0.7; 
+        const ground = MeshBuilder.CreateGround("ground",  
+            {width: 6, height: 6}, scene); 
 
-    Insepector.Show(scene, {});
+        // Détachement du contrôle clavier de la caméra pour éviter les mouvements indésirables
+        camera.inputs.attached.keyboard.detachControl();
 
-    engine.runRenderLoop(function () {
-        scene.render();
+        window.addEventListener("keydown", (event) => {
+            let moveDistance = 0.25;
+            switch (event.key) {
+                case "ArrowDown":
+                    sphere.position.z -= moveDistance;
+                    break;
+                case "ArrowUp":
+                    sphere.position.z += moveDistance;
+                    break;
+                case "ArrowLeft":
+                    sphere.position.x -= moveDistance;
+                    break;
+                case "ArrowRight":
+                    sphere.position.x += moveDistance;
+                    break;
+            }
+            // La caméra suit la sphère
+            camera.position.x = sphere.position.x;
+            camera.position.z = sphere.position.z - 10; // Maintient la caméra à une distance constante derrière la sphère
         });
 
-    window.addEventListener("resize", function () {
-        engine.resize();
-        });
-}
+        document.getElementById("fps").innerText = "FPS: " + engine.getFps().toFixed();
+        setInterval(() => {
+            document.getElementById("fps").innerText = "FPS: " + engine.getFps().toFixed();
+        }, 1000);
 
-var createScene = function () {
-    // This creates a basic Babylon Scene object (non-mesh)
-    var scene = new Scene(engine);
-
-    // This creates and positions a free camera (non-mesh)
-    var camera = new FreeCamera("camera1", new Vector3(0, 5, -10), scene);
-
-    // This targets the camera to scene origin
-    camera.setTarget(Vector3.Zero());
-
-    // This attaches the camera to the canvas
-    camera.attachControl(canvas, true);
-
-    // This creates a light, aiming 0,1,0 - to the sky (non-mesh)
-    var light = new HemisphericLight("light", new Vector3(0, 1, 0), scene);
-
-    // Default intensity is 1. Let's dim the light a small amount
-    light.intensity = 0.7;
-
-    // Our built-in 'sphere' shape.
-    var sphere = MeshBuilder.CreateSphere("sphere", {diameter: 2, segments: 32}, scene);
-
-    // Move the sphere upward 1/2 its height
-    sphere.position.y = 1;
-
-    // Our built-in 'ground' shape.
-    var ground = MeshBuilder.CreateGround("ground", {width: 6, height: 6}, scene);
-
-    return scene;
+        return scene; 
+    }; 
+    const scene = createScene();  
+    engine.runRenderLoop(function () { 
+            scene.render(); 
+    }); 
+    window.addEventListener("resize", function () { 
+            engine.resize(); 
+    }); 
 };
