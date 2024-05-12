7d0eccc33e3ad0cb3e705c00dacb56c091b2caef
jeanlucngo <120799247+jeanlucngo@users.noreply.github.com>
2024-05-12 20:43:16 +0200

    6nd commit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import { Engine, FreeCamera, HemisphericLight, MeshBuilder, Scene, SceneLoader, Vector3 } from "@babylonjs/core";
-import Game from "./game_arc";
 import Game_arc from "./game_arc";
+import Game_course from "./game_course";
 
 let engine;
 let canvas;
-let game;
+let game1;
+let game2;
+let game3;
 
 window.onload = () => {
     canvas = document.getElementById("renderCanvas");
@@ -15,7 +17,11 @@ window.onload = () => {
         engine.resize();
     });
     
-    game = new Game_arc(engine, canvas);
-    game.init();
-    game.start();
+    /*game1 = new Game_arc(engine, canvas);
+    game1.init();
+    game1.start();*/
+
+    game2 = new Game_course(engine, canvas);
+    game2.init2();
+    game2.start2();
 }
